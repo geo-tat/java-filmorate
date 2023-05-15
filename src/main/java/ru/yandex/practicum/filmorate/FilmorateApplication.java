93be993a31914f60e2766db3c9e96e3ac4136d4a
Georgii <nosleep2night@yandex.ru>
2023-05-15 20:28:43 +0400

    TЗ - 9

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,8 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.yandex.practicum.filmorate.controller.FilmController;
-import ru.yandex.practicum.filmorate.model.Film;
 
 @SpringBootApplication
 public class FilmorateApplication {
