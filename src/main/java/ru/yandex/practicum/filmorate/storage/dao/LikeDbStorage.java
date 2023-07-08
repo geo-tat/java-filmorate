7cc78bd17568268729c531ca5e1e5fa4425b0001
olgaobraztsova <olga.obraztsova@gmail.com>
2023-07-08 16:18:19 +0530

    fix: replaced arraylists in search() and getRecommendations() by Collection

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/LikeDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/LikeDbStorage.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.storage.LikeStorage;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
